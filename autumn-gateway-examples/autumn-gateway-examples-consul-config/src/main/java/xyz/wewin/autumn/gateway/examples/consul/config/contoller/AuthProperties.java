72bed1669a76a8e1c42f38c140fdec02dc3f4f08
Percy0601 <745356640@qq.com>
2026-07-04 18:14:56 +0800

    consul config refresh

--- autumn-gateway-examples/autumn-gateway-examples-consul-config/src/main/java/xyz/wewin/autumn/gateway/examples/consul/config/contoller/AuthProperties.java
+++ autumn-gateway-examples/autumn-gateway-examples-consul-config/src/main/java/xyz/wewin/autumn/gateway/examples/consul/config/contoller/AuthProperties.java
@@ -1,7 +1,6 @@
 package xyz.wewin.autumn.gateway.examples.consul.config.contoller;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
