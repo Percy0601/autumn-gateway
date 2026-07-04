691ea4077ff4ec20645934d01ca2a6446f6addbc
Percy0601 <745356640@qq.com>
2026-07-04 09:22:30 +0800

    consul

---
+++ autumn-gateway-examples/autumn-gateway-examples-consul/src/main/java/xyz/wewin/autumn/gateway/examples/consul/ConsulAutumnGatewayApplication.java
@@ -0,0 +1,14 @@
+package xyz.wewin.autumn.gateway.examples.consul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsulAutumnGatewayApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(ConsulAutumnGatewayApplication.class, args);
+	}
+
+
+}
