9e0005e76f71822e38d8e3c8f66d9690b359ba3f
Percy0601 <745356640@qq.com>
2026-07-12 23:48:56 +0800

    dubbo-rest

--- autumn-gateway-examples/autumn-gateway-examples-service-dubbo-a/src/main/java/xyz/wewin/autumn/gateway/examples/dubbo/api/UserService.java
+++ autumn-gateway-examples/autumn-gateway-examples-service-dubbo-a/src/main/java/xyz/wewin/autumn/gateway/examples/dubbo/api/UserService.java
@@ -1,17 +1,15 @@
 package xyz.wewin.autumn.gateway.examples.dubbo.api;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 @RequestMapping("/api/users")
 public interface UserService {
-
-    @GetMapping("/{id}")
+    
+    @GetMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
     UserDTO getById(@PathVariable("id") Long id);
 
     // dubbo RPC 专用方法（不走 REST）
