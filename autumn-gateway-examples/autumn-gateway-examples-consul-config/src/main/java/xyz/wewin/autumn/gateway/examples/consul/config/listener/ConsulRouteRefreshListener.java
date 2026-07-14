c197b7124b3f43e3c07563aefc184aa65a12efb5
Percy0601 <745356640@qq.com>
2026-07-14 16:38:33 +0800

    动态路由，整合配置中心，动态路由

---
+++ autumn-gateway-examples/autumn-gateway-examples-consul-config/src/main/java/xyz/wewin/autumn/gateway/examples/consul/config/listener/ConsulRouteRefreshListener.java
@@ -0,0 +1,25 @@
+package xyz.wewin.autumn.gateway.examples.consul.config.listener;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.cloud.context.environment.EnvironmentChangeEvent;
+import org.springframework.cloud.gateway.event.RefreshRoutesEvent;
+import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ConsulRouteRefreshListener {
+    private Logger log = LoggerFactory.getLogger(this.getClass());
+    private final ApplicationEventPublisher publisher;
+
+    public ConsulRouteRefreshListener(ApplicationEventPublisher publisher) {
+        this.publisher = publisher;
+    }
+
+    @EventListener(EnvironmentChangeEvent.class)
+    public void onConfigChange(EnvironmentChangeEvent event) {
+        log.info("Consul Config changed keys: {}, refreshing routes...", event.getKeys());
+        publisher.publishEvent(new RefreshRoutesEvent(this));
+    }
+}
